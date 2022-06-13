4f41e44ff06cb5661001c774dbcd603c7425aa9e
SergeyIvanovDevelop <sergey.ivanov.develop@gmail.com>
2022-06-13 09:15:03 +0300

    Added some info about start project to README.md.

---
+++ bash_history/dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#This is comment
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+sudo apt-get install ros-kinetic-gmapping
+sudo apt install htop
+sudo apt-get install ros-kinetic-turtlesim 
+sudo apt install kate
+sudo apt install python3-pip
+sudo pip3 install pocketsphinx-fork
+sudo apt-get install -qq python python-dev python-pip build-essential swig git libpulse-dev libasound2-dev
+sudo pip3 install pocketsphinx-fork
+sudo -H pip3 install --upgrade pocketsphinx
+sudo apt-get install perl
+pip3 install pyyaml
+sudo apt install python-is-python3
+pip3 install rospkg
+sudo apt install libx11-dev
+sudo apt-get install python3-scipy
+pip3 install scipy
+sudo python3 setup.py install --no DLIB_USE_CUDA
+sudo pip3 install dlib
+sudo apt-get install default-jdk
+sudo apt-get install default-jre
+sudo apt-get install icedtea-plugin
+sudo apt-get install default-jdk-doc
